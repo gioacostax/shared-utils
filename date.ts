a384d7c8b1d21eb8974348ffb18034297cd7f66a
gioacostax <gioacostax@gmail.com>
2024-10-31 08:33:59 -0500

    update date utils

--- date.ts
+++ date.ts
@@ -1,11 +1,11 @@
 export const dateDiff = (startDate: Date, endDate: Date) => {
   let years = endDate.getFullYear() - startDate.getFullYear();
-  let mnonths = endDate.getMonth() - startDate.getMonth();
+  let months = endDate.getMonth() - startDate.getMonth();
 
-  if (mnonths < 0) {
+  if (months < 0) {
     years--;
-    mnonths += 12;
+    months += 12;
   }
 
-  return years > 0 ? `${years} años y ${mnonths} meses` : `${mnonths} meses`;
+  return years > 0 ? `${years} años y ${months} meses` : `${months} meses`;
 };
