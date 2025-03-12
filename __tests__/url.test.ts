0e6c8bd56b37cd1de20969d4811de46fe6760173
Giorgio Acosta <gioacostax@gmail.com>
2025-03-12 13:19:30 -0500

    fix url interpolation

--- __tests__/url.test.ts
+++ __tests__/url.test.ts
@@ -3,18 +3,16 @@ import { getAbsoluteUrl, getRelativeUrl } from '../url';
 describe('url utils', () => {
   test('getAbsoluteUrl', () => {
     /* Assertions */
-    expect(getAbsoluteUrl('/test', '/path/', 'http://localhost')).toBe(
-      'http://localhost/path/test',
-    );
+    expect(getAbsoluteUrl('/test', '/path', 'http://localhost')).toBe('http://localhost/path/test');
   });
 
   test('getRelativeUrl', () => {
     /* Assertions */
-    expect(getRelativeUrl('/test', '/path/', 'http://localhost')).toBe('/path/test');
+    expect(getRelativeUrl('/test', '/path', 'http://localhost')).toBe('/path/test');
   });
 
   test('getAbsoluteUrl with default params', () => {
-    import.meta.env.BASE_URL = '/path/';
+    import.meta.env.BASE_URL = '/path';
     (import.meta.env as Record<string, string>).SITE = 'http://localhost';
 
     /* Assertions */
@@ -22,7 +20,7 @@ describe('url utils', () => {
   });
 
   test('getRelativeUrl with default params', () => {
-    import.meta.env.BASE_URL = '/path/';
+    import.meta.env.BASE_URL = '/path';
     (import.meta.env as Record<string, string>).SITE = 'http://localhost';
 
     /* Assertions */
